3bf2d820541c9b369f124d08f674304bd88c9e75
sujan majumder <sujanmajumder211@gmail.com>
2019-07-03 11:39:32 +0600

    medicin

---
+++ index.php
@@ -0,0 +1,478 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="icon" href="./images/57393079_2430661033644612_4135170164419198976_n.png" sizes="16x16" type="image/png">  
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css">
+<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="css/style.css">
+<title>Medicine Data - Entry</title>
+</head>
+<body>
+
+<!-- start header -->
+<section id="header">
+<form>
+ <div class="container">
+        <div class="row">
+            <div class="col-md-2" id="logo">
+                <img src="./images/57393079_2430661033644612_4135170164419198976_n.png" alt="">
+            </div>
+            <div class="col-md-10" id="header_text">
+            <p>  <button type="submit" class="btn btn-primary">+ Add</button></p>
+             
+                <h6>Prescription Entry </h6>
+            </div>
+        </div>
+    </div>
+    </section>
+    <hr>
+
+
+<!-- end header -->
+
+<section id="sidebar">
+    <div class="container-fluid">
+        <div class="row">
+        <div class="col-md-2" id="sidebar_nav">
+        <div class="wrapper">
+    <!-- Sidebar -->
+    <nav id="sidebar">
+        
+
+        <ul class="list-unstyled components">
+            <p>Prescription</p>
+            <li>
+                <a href="#">Doctor</a>
+            </li>
+            <li>
+                <a href="#">Present</a>
+            </li>
+            
+            <li>
+                <a href="#">Prescription</a>
+            </li>
+            <li>
+                <a href="#">Medicine</a>
+            </li>
+        </ul>
+    </nav>
+
+</div>
+        </div>
+        <!-- form-section-1-start -->
+        <div class="col-md-4" id="sidebar_form">
+        
+            <h4>Form No Information :</h4>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                     <input type="text" class="form-control"  value="0000">
+                </div>
+                <div class="form-group col-md-6">
+                <input type="text" class="form-control"  value="AbedinIt">
+                </div>
+            </div>
+            <div class="form-row">
+            Form No :
+            <div class="form-group col">
+            <input type="text" class="form-control"  >
+                
+            </div>
+            <div class="form-group col">
+                <input type="text" class="form-control"  >
+                
+            </div>
+            <div class="form-group col">
+                <input type="text" class="form-control"  >
+                
+            </div>
+            </div>
+            <div class="form-row">
+            Call Date :
+            <div class="form-group col-md-3">
+                
+                <input type="date" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+            </div>
+            Pres Date :
+            <div class="form-group col-md-3">
+                
+                <input type="date" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+            </div>
+            </div>
+            <div class="form-row">
+                Route  :
+                <div class="form-group col">
+                <select id="inputState" class="form-control">
+                    <option selected>Choose...</option>
+                    <option>...</option>
+                </select>
+                </div>
+                <div class="form-group col">
+                <input type="text" class="form-control" id="inputZip">
+                </div>
+            </div>
+            <div class="form-row">
+                District  :
+                <div class="form-group col">
+                <select id="inputState" class="form-control">
+                    <option selected>Choose...</option>
+                    <option>Dhaka</option>
+                    <option>chittagong</option>
+                    <option>Feni</option>
+                    <option>Foriedpur</option>
+                </select>
+                </div>
+                <div class="form-group col">
+                <input type="text" class="form-control" id="inputZip">
+                </div>
+            </div>
+            <div class="form-row">
+                Thana  :
+                <div class="form-group col">
+                <select id="inputState" class="form-control">
+                    <option selected>Choose...</option>
+                   
+                </select>
+                </div>
+                <div class="form-group col">
+                <input type="text" class="form-control" id="inputZip">
+                </div>
+            </div>
+
+            <div class="form-row">
+                Union  :
+                <div class="form-group col">
+                <select id="inputState" class="form-control">
+                    <option selected>Choose...</option>
+                    
+                </select>
+                </div>
+                <div class="form-group col">
+                <input type="text" class="form-control" id="inputZip">
+                </div>
+            </div>
+
+            <h5>Doctor Information </h5>
+            
+            <div class="form-row">
+            Doctor SL No  :
+                <div class="form-group col">
+                <input type="text" class="form-control">
+                </div>
+                <div class="form-group col">
+                <input class="icon-refresh" type="submit" value="Refresh" name="Test"> </>
+                </div>
+                RX Type  :
+                <div class="form-group col">
+                <select id="inputState" class="form-control">
+                    <option selected>Choose...</option>
+                    
+                </select>
+                </div>
+                 
+            </div>
+            <div class="form-row">
+                Doctor  :
+                <div class="form-group col">
+                <select id="inputState" class="form-control">
+                    <option selected>Choose...</option>
+                    
+                </select>
+                </div>
+                <div class="form-group col">
+                <input type="text" class="form-control" id="inputZip">
+                </div>
+            </div>
+
+
+           
+        
+
+        </div>
+        <!-- form-section-1-end -->
+
+
+        <!-- form-section-2-start -->
+         <!-- form-section-1-start -->
+         <div class="col-md-4" id="sidebar_form">
+       
+            <br> <br> 
+        <div class="form-row">
+                Village  :
+                <div class="form-group col">
+                <select id="inputState" class="form-control">
+                    <option selected>Choose...</option>
+                    <option>...</option>
+                </select>
+                </div>
+                <div class="form-group col">
+                <input type="text" class="form-control" id="inputZip">
+                </div>
+        </div>
+        <div class="form-row">
+                Region  :
+                <div class="form-group col-md-6">
+                <input type="text" class="form-control" id="inputZip">
+
+                </div>
+                <div class="form-group col-md-2">
+                <input type="text" class="form-control" id="inputZip">
+                </div>
+                <div class="form-group col-md-2">
+                <input type="text" class="form-control" id="inputZip">
+                </div>
+        </div>
+        <div class="form-row">
+                Area  :
+                <div class="form-group col-md-6">
+                <input type="text" class="form-control" id="inputZip">
+
+                </div>
+                <div class="form-group col">
+                <input type="text" class="form-control" id="inputZip">
+                </div>
+        </div>
+        <div class="form-row">
+                Teritori  :
+                <div class="form-group col-md-6">
+                <input type="text" class="form-control" id="inputZip">
+
+                </div>
+                <div class="form-group col">
+                <input type="text" class="form-control" id="inputZip">
+                </div>
+        </div>
+        <div class="form-row">
+                Market  :
+                <div class="form-group col">
+                <select id="inputState" class="form-control">
+                    <option selected>Choose...</option>
+                    <option>...</option>
+                </select>
+                </div>
+                <div class="form-group col">
+                <input type="text" class="form-control" id="inputZip">
+                </div>
+                <div class="form-group col">
+                <input class="icon-refresh" type="submit" value="Refresh" name="Test"> </>
+                </div>
+        </div>
+
+        <div class="form-row">
+                Inst/Chemist/Flag   :
+                <div class="form-group col-md-6">
+                <select id="inputState" class="form-control">
+                    <option selected>Choose...</option>
+                    <option>...</option>
+                </select>
+
+                </div>
+                <div class="form-group col">
+                <input type="text" class="form-control" id="inputZip">
+                </div>
+        </div>
+
+        <div class="form-row">
+                Inst/Chemist  :
+                <div class="form-group col-md-6">
+                <select id="inputState" class="form-control">
+                    <option selected>Choose...</option>
+                    <option>...</option>
+                </select>
+
+                </div>
+                <div class="form-group col">
+                <input type="text" class="form-control" id="inputZip">
+                </div>
+
+            <div class="form-group col-md-6">
+            <label for="inputEmail4">Indication</label>
+            <select id="inputState" class="form-control">
+                    <option selected>Choose...</option>
+                    <option>...</option>
+                </select>
+                <input class="icon-refresh text-left" type="submit" value="Refresh" name="Test"> </>
+                
+                
+            </div>
+            <div class="form-group col-md-6">
+            <input type="text" class="form-control" id="inputZip">
+            <input type="text" class="form-control" value="indication">
+            <input type="text" class="form-control" id="inputZip">
+
+            </div>
+            
+        </div>
+        
+
+
+         
+        
+
+        </div>
+
+        <!-- form-section-2-end -->
+        </div>
+    </div>
+    </section>
+
+    <section id="brand" class="offset-2" >
+    
+
+
+        <div class="container-fluid " id="brand_id">
+            <div class="row">
+
+
+            <table class="table table-bordered table-condensed">
+    <p class="pp pt-5 ">Entry-List</p>
+            
+                <thead>
+                    <th>Brand Name</th>
+                    <th>Dosage</th>
+                    <th>strength</th>
+                    <th>Qty</th>
+                    <th>Generic</th>
+                    <th>company</th>
+                    <th>B-code</th>
+                    <th>G-code</th>
+                    <th>T-code</th>
+                    <th>C-code</th>
+
+                </thead>
+    <tbody>
+   
+        <tr>
+           <td><input type="text" class="form-control" /></td>
+           <td><input type="text" class="form-control" /></td>
+           <td><input type="text" class="form-control" /></td>
+           <td><input type="text" class="form-control" /></td>
+           <td><input type="text" class="form-control" /></td>
+           <td><input type="text" class="form-control" /></td>
+           <td><input type="text" class="form-control" /></td>
+           <td><input type="text" class="form-control" /></td>
+           <td><input type="text" class="form-control" /></td>
+           <td><input type="text" class="form-control" /></td>
+        </tr>
+    </tbody>
+</table>
+            </div>
+        </div>
+    </section>
+
+    <section id="last_table">
+        <div class="container-fluid" id="brand_id1">
+            <div class="row">
+            <div class="container">
+                <h4>Brand Information :</h4>
+                <h5 class="text-center"> Brand Detals</h5>
+                <table class="table">
+                    <thead>
+                        
+                    <tr>
+                        <th></th>
+                        <th>Sl</th>
+                        <th>Brand Name</th>
+                        <th>Dosage</th>
+                        <th>strength</th>
+                        <th>Qty</th>
+                        <th>Generic</th>
+                        <th>Company</th>
+                        <th>B-code</th>
+                        <th>G-code</th>
+                        <th>T-code</th>
+                        <th>C-code</th>
+                       
+                    </tr>
+                    </thead>
+                    <tbody>
+                    
+                   
+                    <tr>
+                        <td>*</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td>
+                        <button onclick="myFunction()" class="btn btn-danger">Double click to Delete </button>
+                        <script>
+                            function myFunction()
+                            {
+                            var aForm = document.forms["aForm"].getElementsByTagName("input");
+                            var len = aForm.length;
+                            console.log("Form has "+len+" input fields.");
+
+                            for (var i = 0; i < len; i++)
+                            {
+                                aForm[i].remove();
+                                console.log("Removed number: "+i);
+                            }
+
+                            console.log("After the for loop: "+ aForm.length);
+                            }
+                            </script>
+                        
+                        </td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        
+                        
+                        
+                    </tr>
+                    </tbody>
+                </table>
+
+                <div class="footer-scetion">
+                    <div class="container-fluid">
+                        <div class="row">
+                        
+                        <nav class="navbar navbar-expand-sm bg-light">
+                            <button type="submit" class="btn btn-primary mr-2 ">Add</button>
+
+                        <!-- Links -->
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                        <a href="#"  class="  ">save and next Pres</a>
+                           
+                            </li>
+                            <li class="nav-item">
+                            <a href="#"  class="  ">save and next form</a>
+                            </li>
+                            <li class="nav-item">
+                            <a href="#"  class="  ">Revert</a>
+                            </li>
+                            <li class="nav-item">
+                            <a href="#"  class="  ">Exit</a>
+                            </li>
+                        </ul>
+
+                        
+                        <p>Error Message</p>
+                        
+                        </div>
+                    </div>
+                </div>
+               
+            </div>
+        </div>
+    </section>
+    </form>
+
+  
+    
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.15.0/umd/popper.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
